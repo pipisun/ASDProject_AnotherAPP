914497b034f38e8436a871bed1388ce6e18f3971
Jeremy <sst1978@163.com>
2017-08-09 15:30:32 -0500

    Put the reasonable data in it

--- src/main/java/framework/sql/inital.sql
+++ src/main/java/framework/sql/inital.sql
@@ -1,4 +1,4 @@
---drop table `users`
+-- drop table `users`
 
 CREATE TABLE `users` (
   `userid` int(20) UNSIGNED NOT NULL,
@@ -15,24 +15,17 @@ CREATE TABLE `users` (
   `dateupdated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- 转存表中的数�?� `users`
---
-
 INSERT INTO `users` (`userid`, `fullname`, `gender`, `state`, `city`, `street`, `zipcode`, `birthyear`, `email`, `password`, `datecreated`, `dateupdated`) VALUES
-(1, 'aa', 0, 'aa', 'aa', '21', 21, 1998, 'aa', 'aa', '2017-03-18 16:32:11', '2017-03-18 16:32:11'),
-(2, 'bb', 1, 'bb', 'bb', '23', 23, 1991, 'bb', 'bb', '2017-03-18 16:32:41', '2017-03-18 16:32:41'),
-(3, 'cc', 1, 'cc', 'cc', '26', 24, 1992, 'cc', 'cc', '2017-03-18 16:33:11', '2017-03-18 16:33:11'),
-(4, 'dd', 0, 'dd', 'dd', '59', 12, 1999, 'dd', 'dd', '2017-03-18 16:33:44', '2017-03-18 16:33:44'),
-(5, 'ee', 1, 'ee', 'ee', '51', 12, 1980, 'ee', 'ee', '2017-03-18 16:34:18', '2017-03-18 16:34:18'),
-(12, 'aa', 0, 'aa', 'aa', '21', 0, 0, 'aa', 'aa', '2017-03-19 16:02:50', '2017-03-19 16:02:50'),
-(13, 'aa', 0, 'aa', 'aa', '21', 0, 0, 'aa', 'aa', '2017-03-19 16:03:59', '2017-03-19 16:03:59'),
-(14, 'aa', 0, 'aa', 'aa', '21', 0, 0, 'aa', 'aa', '2017-03-19 16:17:26', '2017-03-19 16:17:26');
+(1, 'Alice A', 0, 'FL', 'Miami', '1 St', 31200, 1998, 'aa', 'aa', '2017-03-18 16:32:11', '2017-03-18 16:32:11'),
+(2, 'Bily B', 1, 'IL', 'Chicago', '2 St', 41500, 1991, 'bb', 'bb', '2017-03-18 16:32:41', '2017-03-18 16:32:41'),
+(3, 'Cindy C', 0, 'NY', 'NYC', '26 Ave', 12110, 1980, 'cc', 'cc', '2017-03-18 16:33:11', '2017-03-18 16:33:11'),
+(4, 'David D', 1, 'CA', 'San Jose', '59 St', 21000, 1999, 'dd', 'dd', '2017-03-18 16:33:44', '2017-03-18 16:33:44'),
+(5, 'Elena E', 0, 'NC', 'Charlotte', '51 St', 35498, 1980, 'ee', 'ee', '2017-03-18 16:34:18', '2017-03-18 16:34:18');
 
 ALTER TABLE `users`
   ADD PRIMARY KEY (`userid`);
 ALTER TABLE `users`
-  MODIFY `userid` int(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `userid` int(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
  
 
 CREATE TABLE `permission` (
@@ -58,9 +51,7 @@ INSERT INTO `permission` (`permissionid`, `permissioncode`, `permissionname`,`de
 (7, 'BankMainWindow.Menu3.Item31', 'Display User', 'Display User'),
 (8, 'BankMainWindow.Menu3.Item32', 'Add User', 'Add User'),
 (9, 'BankMainWindow.Menu3.Item33', 'Manage Permission', 'Manage Permission'),
-(10, 'BankMainWindow.Menu4.Item41', 'Contents', 'Contents'),
-(11, 'BankMainWindow.Menu4.Item42', 'About...', 'About...'),
-(12, 'BankMainWindow.Menu4.Item43', 'Exit', 'Exit');
+(10, 'BankMainWindow.Menu4.Item43', 'Exit', 'Exit');
  
  
  
@@ -74,26 +65,8 @@ CREATE TABLE userpermission (
         PRIMARY KEY (userpermissionid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-insert into userpermission(permissionid,userid) value(1,18);
-insert into userpermission(permissionid,userid) value(2,18);
-
-
-
-
-
-  
-  
- --- test for sql 
-  update users set fullname='linchen', email = 'linchen@gmail.com',password='linchen'  where userid=18
-  
-select p.* from userpermission up left join users u on up.userid=u.userid
-left join permission p on up.permissionid = p.permissionid
-
-where up.userid =18
-  
-  select * from userpermission
-  select * from users;
-  select * from permission;
-  delete   from userpermission;
-  
-  select * from  USERS  where 1 = 1 and zipcode=0 and password='test' and email='test@gmail.com'
+insert into userpermission(permissionid,userid) value(1,4);
+insert into userpermission(permissionid,userid) value(2,4);
+insert into userpermission(permissionid,userid) value(3,4);
+insert into userpermission(permissionid,userid) value(4,4);
+insert into userpermission(permissionid,userid) value(5,4);
