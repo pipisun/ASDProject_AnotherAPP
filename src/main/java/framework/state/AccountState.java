f717ad014a89a1e9ce76ed00e75afedc7567da98
YifengZhong <yf_zhong@hotmail.com>
2017-08-10 12:16:52 -0500

    chenge State interface

--- src/main/java/framework/state/AccountState.java
+++ src/main/java/framework/state/AccountState.java
@@ -4,8 +4,8 @@ package framework.state;
  * Created by yf_zh on 2017/08/09.
  */
 public interface AccountState {
-    public void deposit(double amount);
-    public boolean withdraw(double amount);
+    public void deposit(double amount, String accnr);
+    public boolean withdraw(double amount, String accnr);
     public void computeInterest();
     public void stateCheck(double amount);
 }
